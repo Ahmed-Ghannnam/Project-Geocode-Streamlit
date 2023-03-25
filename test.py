6aee191fc3c11e4464dd85cbbb5d562f946936eb
Ahmed-Ghannnam <127568672+Ahmed-Ghannnam@users.noreply.github.com>
2023-03-25 16:32:11 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 # Initialize Nominatim geocoder
 import geopandas as gpd
-import pandas as pd
 import streamlit as st
 from geopy.geocoders import Nominatim
 import folium
@@ -29,7 +28,7 @@ if location:
 
 
     # create a Pandas DataFrame that contains the address I wish to geocode:
-    home = pd.DataFrame([{'address': address}])
+    home = gpd.GeoDataFrame([{'address': address}])
     home
 
 
@@ -37,16 +36,26 @@ if location:
     home = gpd.tools.geocode(home['address'], Nominatim, user_agent='Isochrone calculator')
     home
 
-    st.write("this buffer 5km around your home")
-
-    # we will create our bubble by projecting to the GDA2020 MGA Zone 55 projection (EPSG code 7855) to create the buffer before re-projecting back to the original GCS WGS84 projection
-    buffer = home.to_crs(epsg=7855).buffer(5000).to_crs(epsg=3857)
-
     # Create a folium map centered at the geocoded location
-    map = folium.Map(location=[location.latitude, location.longitude], zoom_start=15)
+    map = folium.Map(location=[location.latitude, location.longitude], zoom_start=12)
+
+    length = st.text_input("Enter Distance of buffer:")
+    st.write("The Defult value of Buffer 5000 m")
+    bu=st.button("make buffer")
+    if bu:
+        if not length:
+            length =5000
+        length= int(length)
+        # we will create our bubble by projecting to the GDA2020 MGA Zone 55 projection (EPSG code 7855) to create the buffer before re-projecting back to the original GCS WGS84 projection
+        buffer = home.to_crs(epsg=7855).buffer(length).to_crs(epsg=3857)
 
     # put buffer on map
-    folium.GeoJson(buffer).add_to(map)
+        folium.GeoJson(buffer).add_to(map)
+
+        geojson=buffer.to_json()
+        file_name ="polygon buffer.geojson"
+        st.download_button(label="Download Buffer GeoJSON",data=geojson,file_name=file_name,mime="application/json")
+
 
     # put geometry on the same map
     folium.GeoJson(home, tooltip=folium.GeoJsonTooltip(['address'])).add_to(map)
@@ -54,15 +63,14 @@ if location:
     # Add a marker and popup for the geocoded location
     folium.Marker([location.latitude, location.longitude],popup=address).add_to(map)   
     
-    
-    geojson=buffer.to_json()
-    file_name ="polygon buffer.geojson"
-    st.download_button(label="Download Buffer GeoJSON",data=geojson,file_name=file_name,mime="application/json")
-
     # Display the map
     folium_static(map)
+
 else:
     if address :
         st.write("Location not found.")
+    
+
+
 
 
